90c2fcc96151a576fa892e01e562a9c3fee33005
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-04-04 00:12:35 +0200

    created python driver

---
+++ SDALib-CPP/SDALib-Python/Driver.py
@@ -0,0 +1,8 @@
+
+
+class SDADriver:
+    def InitAI(self):
+        return
+
+    def UpdateAI(self):
+        return (0.4, 0.2, 0.5, 1)
---
+++ SDALib-CPP/SDALib-Python/PythonDriver.cpp
@@ -0,0 +1,56 @@
+#include "PythonDriver.h"
+
+
+PythonDriver::PythonDriver()
+    :m_main(import("__main__"))
+    ,m_globals(m_main.attr("__dict__"))
+{
+    exec("exec(open(\"Driver.py\").read())", m_globals);
+    m_SDADriverType = m_globals["SDADriver"];
+    m_pythonDriver = m_SDADriverType();
+    m_updateFunction = m_pythonDriver.attr("UpdateAI");
+    m_initFunction = m_pythonDriver.attr("InitAI");
+}
+
+void PythonDriver::InitAI()
+{
+    try
+    {
+        m_initFunction();
+    }
+    catch (error_already_set)
+    {
+        PyErr_Print();
+    }
+}
+
+SDAAction PythonDriver::UpdateAI(SDAData& p_data)
+{
+    SDAAction action;
+    try
+    {
+        tuple result = extract<tuple>(m_updateFunction()); // needs input data
+        action.Steer = extract<float>(result[0]);
+        action.Accel = extract<float>(result[1]);
+        action.Brake = extract<float>(result[2]);
+        action.Gear  = extract<int>(result[3]);
+    }
+    catch (error_already_set)
+    {
+        PyErr_Print();
+    }
+
+    return action;
+}
+
+
+int main()
+{
+    Py_Initialize();
+
+    PythonDriver driver;
+    driver.Run();
+
+    Py_Exit(0);
+    return 0;
+}
