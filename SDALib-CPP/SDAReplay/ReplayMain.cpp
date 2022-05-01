ac3ff0c8a6f447f75521fd6851d6e7b78a5cd259
MaikVink <57708260+MaikVink@users.noreply.github.com>
2022-05-01 14:30:45 +0200

    change to read command line arguments

--- SDALib-CPP/SDAReplay/ReplayMain.cpp
+++ SDALib-CPP/SDAReplay/ReplayMain.cpp
@@ -1,8 +1,16 @@
 #include "ReplayDriver.h"
 
-int main()
+int main(int argc, char **path)
 {
-    std::string replayFileName("ReplayData/bbRecording20220330-123618.txt");
+    std::string replayFileName("ReplayData/bbRecording20220330-123618.txt"));
+    for (int i = 1; i < argc; i++)
+    {
+        if (path[i] == std::string("--bbfile"))
+        {
+            std::string test(path[++i]);  //"ReplayData/bbRecording20220330-123618.txt");
+            replayFileName = test;
+        }
+    }
 
     ReplayDriver driver(replayFileName);
     driver.Run();
