4d448b0b5c747f930b5ff4b8cdc56140a47d5394
DragonWolf2b <57708260+MaikVink@users.noreply.github.com>
2022-05-04 12:17:35 +0200

    Update SDALib-CPP/SDAReplay/ReplayMain.cpp
    Co-authored-by: KungFuDonkey <43845930+KungFuDonkey@users.noreply.github.com>

--- SDALib-CPP/SDAReplay/ReplayMain.cpp
+++ SDALib-CPP/SDAReplay/ReplayMain.cpp
@@ -31,7 +31,7 @@ int main(int p_argc, char **p_path)
     if (replayFileName.empty())  // if you don't give the program a filename, print the following error.
     {
         std::cerr << "no replay file found, please use command \"--bbfile <filename>\"" << std::endl;
-        return 0;
+        return 2;
     }
 
     ReplayDriver driver(replayFileName);
