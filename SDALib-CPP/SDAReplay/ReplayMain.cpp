658e750da1e6abe34c38119fe50eb3e59fd6b4f3
MaikVink <57708260+MaikVink@users.noreply.github.com>
2022-05-04 11:28:37 +0200

    refactor command line arguments with boost

--- SDALib-CPP/SDAReplay/ReplayMain.cpp
+++ SDALib-CPP/SDAReplay/ReplayMain.cpp
@@ -1,14 +1,36 @@
 #include "ReplayDriver.h"
+#include <boost/program_options.hpp>
+
+using namespace boost::program_options;
 
 int main(int p_argc, char **p_path)
 {
-    std::string replayFileName;  // initialize the string
-    for (int i = 1; i < p_argc; i++)
-    {
-        // picks the argument written after --bbfile
-        if (strcmp(p_path[i], "--bbfile") == 0) continue;
+    variables_map vm;
+    options_description desc{"CMDagruments"};
+    desc.add_options()("bbfile", value<std::string>(), "BlackBoxReplayFile");
 
-        replayFileName = p_path[++i];
+    //tries an command line argument, if does not exist store crashes, so it prints out a message.
+    try
+    {
+        store(parse_command_line(p_argc, p_path, desc), vm);   
+    }
+    catch (const error &er)
+    {
+        std::cerr << "argument is invalid, try: \"--bbfile\" instead" << std::endl;
+        return 0;
+    }
+
+
+    std::string replayFileName;  // initialize the string
+    if (vm.count("bbfile"))
+    {
+        replayFileName = vm["bbfile"].as<std::string>();
+    }
+
+    if (replayFileName.empty())
+    {
+        std::cerr << "no replay file found, please use command \"--bbfile <filename>\"" << std::endl;
+        return 0;
     }
 
     ReplayDriver driver(replayFileName);
