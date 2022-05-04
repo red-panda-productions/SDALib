f3d8f3a8bb320d809f755316ef2eafd069757a4e
MaikVink <57708260+MaikVink@users.noreply.github.com>
2022-05-04 12:40:18 +0200

    changed using namespace to namespace =

--- SDALib-CPP/SDAReplay/ReplayMain.cpp
+++ SDALib-CPP/SDAReplay/ReplayMain.cpp
@@ -1,22 +1,22 @@
 #include "ReplayDriver.h"
 #include <boost/program_options.hpp>
 
-using namespace boost::program_options;
+namespace BsOptions = boost::program_options;
 
 int main(int p_argc, char **p_path)
 {
-    variables_map vm;                                // variables_map that keeps track of the amount of arguments it gets fed through command lines.
-    options_description argOptions{"CMDArguments"};  // initialize the command-line options.
+    BsOptions::variables_map vm;                                // variables_map that keeps track of the amount of arguments it gets fed through command lines.
+    BsOptions::options_description argOptions{"CMDArguments"};  // initialize the command-line options.
 
     // determines all arguments options (name), the value it should have (s) and a description of what it is (description)
-    argOptions.add_options()("bbfile", value<std::string>(), "BlackBoxReplayFile");
+    argOptions.add_options()("bbfile", BsOptions::value<std::string>(), "BlackBoxReplayFile");
 
     // tries to add all command line arguments, if an argument does not exists as an option in "argOptions": print out an error.
     try
     {
         store(parse_command_line(p_argc, p_path, argOptions), vm);
     }
-    catch (const error &er)
+    catch (const BsOptions::error &er)
     {
         std::cerr << "argument is invalid, try: \"--bbfile\" instead" << std::endl;
         return 0;
