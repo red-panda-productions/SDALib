4996eba221832f394f598797be230d84a3c4a2ae
MaikVink <57708260+MaikVink@users.noreply.github.com>
2022-05-04 11:52:07 +0200

    add comments and refactor with clang

--- SDALib-CPP/SDAReplay/ReplayMain.cpp
+++ SDALib-CPP/SDAReplay/ReplayMain.cpp
@@ -5,14 +5,16 @@ using namespace boost::program_options;
 
 int main(int p_argc, char **p_path)
 {
-    variables_map vm;
-    options_description desc{"CMDagruments"};
-    desc.add_options()("bbfile", value<std::string>(), "BlackBoxReplayFile");
+    variables_map vm;                                // variables_map that keeps track of the amount of arguments it gets fed through command lines.
+    options_description argOptions{"CMDArguments"};  // initialize the command-line options.
 
-    //tries an command line argument, if does not exist store crashes, so it prints out a message.
+    // determines all arguments options (name), the value it should have (s) and a description of what it is (description)
+    argOptions.add_options()("bbfile", value<std::string>(), "BlackBoxReplayFile");
+
+    // tries to add all command line arguments, if an argument does not exists as an option in "argOptions": print out an error.
     try
     {
-        store(parse_command_line(p_argc, p_path, desc), vm);   
+        store(parse_command_line(p_argc, p_path, argOptions), vm);
     }
     catch (const error &er)
     {
@@ -20,18 +22,18 @@ int main(int p_argc, char **p_path)
         return 0;
     }
 
-
     std::string replayFileName;  // initialize the string
     if (vm.count("bbfile"))
     {
         replayFileName = vm["bbfile"].as<std::string>();
     }
 
-    if (replayFileName.empty())
+    if (replayFileName.empty())  // if you don't give the program a filename, print the following error.
     {
         std::cerr << "no replay file found, please use command \"--bbfile <filename>\"" << std::endl;
         return 0;
     }
+
     ReplayDriver driver(replayFileName);
     driver.Run();
 
