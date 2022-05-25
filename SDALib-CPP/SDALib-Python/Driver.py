c746dfbc7600813f3c829f8a0de1948cd213f050
Kayleigh Lieverse <k.z.lieverse@students.uu.nl>
2022-05-25 16:55:46 +0200

    Add conversion from C++ to Python for SDATypes and back for the Driver in PythonDriver

--- SDALib-CPP/SDALib-Python/Driver.py
+++ SDALib-CPP/SDALib-Python/Driver.py
@@ -1,23 +1,23 @@
 class SDADriver:
-    def InitAI(self):
+    def __init__(self):
         return
 
-    def UpdateAI(self,SDAData):
+    def UpdateAI(self, SDAData):
         speed = SDAData.car.pub.dynGC.vel.X * 3.6
-        speedLimit = 80 #SDAData.speedLimit
+        speedLimit = 80  # SDAData.speedLimit
 
-        accel = 0 #float between 0 and 1
-        brake = 0 #float between 0 and 1
+        accel = 0  # float between 0 and 1
+        brake = 0  # float between 0 and 1
 
         if speed > speedLimit:
             brake = (speed - speedLimit) / 10
         elif speed < speedLimit:
             accel = (speedLimit - speed) / 10
 
-        #SaveToDatabase(str(brake))
-        #SaveToDatabase(str(accel))
+        # SaveToDatabase(str(brake))
+        # SaveToDatabase(str(accel))
 
-        return 0, self.clamp(accel, 0, 1), self.clamp(brake, 0, 1), 0 #steer, accel, brake, clutch
+        return 0, self.clamp(accel, 0, 1), self.clamp(brake, 0, 1), 0  # steer, accel, brake, clutch
 
     def clamp(self, value, min_value, max_value):
         if value < min_value:
--- SDALib-CPP/SDALib-Python/SDATypes.py
+++ SDALib-CPP/SDALib-Python/SDATypes.py
@@ -1,7 +1,7 @@
 # object containing all data send to the decision maker
 class SDATypes:
     def __init__(self, car, situation, tickCount):
-        self.tickCount = tickCount  # int
+        self.tickCount = tickCount  # long
         self.car = car  # Car
         self.situation = situation  # Situation
 
@@ -22,8 +22,8 @@ class Car:
 # static public car info
 class InitCar:
     def __init__(self, name, sName, codename, teamName, carName, category, raceNumber, startRank, driverType,
-                 networkPlayer, skillLevel, iconColor, dimension, drvPos, bonnetPos, tank, steerLock, statGC, wheel,
-                 visualAttr, masterModel, skinName, skinTargets):
+                 networkPlayer, skillLevel, iconColor1, iconColor2, iconColor3, dimension, drvPos, bonnetPos, tank,
+                 steerLock, statGC, wheel1, wheel2, wheel3, wheel4, visualAttr, masterModel, skinName, skinTargets):
         self.name = name  # string
         self.sName = sName  # string
         self.codename = codename  # string
@@ -35,14 +35,14 @@ class InitCar:
         self.driverType = driverType  # int
         self.networkPlayer = networkPlayer  # int
         self.skillLevel = skillLevel  # int
-        self.iconColor = iconColor  # float[]
+        self.iconColor = [iconColor1, iconColor2, iconColor3]  # float[]
         self.dimension = dimension  # Vector
         self.drvPos = drvPos  # Vector
         self.bonnetPos = bonnetPos  # Vector
         self.tank = tank  # float
         self.steerLock = steerLock  # float
         self.statGC = statGC  # Vector
-        self.wheel = wheel  # WheelSpecifications[]
+        self.wheel = [wheel1, wheel2, wheel3, wheel4]  # WheelSpecifications[]
         self.visualAttr = visualAttr  # VisualAttributes
         self.masterModel = masterModel  # string
         self.skinName = skinName  # string
@@ -61,19 +61,21 @@ class WheelSpecifications:
 
 # static visual attributes
 class VisualAttributes:
-    def __init__(self, exhaustNb, exhaustPos, exhaustPower):
+    def __init__(self, exhaustNb, exhaustPos1, exhaustPos2, exhaustPower):
         self.exhaustNb = exhaustNb  # int
-        self.exhaustPos = exhaustPos  # Vector[]
+        self.exhaustPos = [exhaustPos1, exhaustPos2]  # Vector[]
         self.exhaustPower = exhaustPower  # float
 
 
 # public info on the cars
 class PublicCar:
-    def __init__(self, dynGC, dynGCg, speed, posMat, trkPos, state):
+    def __init__(self, dynGC, dynGCg, speed, posMat1, posMat2, posMat3, posMat4, posMat5, posMat6, posMat7, posMat8,
+                 posMat9, posMat10, posMat11, posMat12, posMat13, posMat14, posMat15, posMat16, trkPos, state):
         self.dynGC = dynGC  # DynamicPoint
         self.dynGCg = dynGCg  # DynamicPoint
         self.speed = speed  # float
-        self.posMat = posMat  # float[,]
+        self.posMat = [[posMat1, posMat2, posMat3, posMat4], [posMat5, posMat6, posMat7, posMat8],
+                       [posMat9, posMat10, posMat11, posMat12], [posMat13, posMat14, posMat15, posMat16]]  # float[][]
         self.trkPos = trkPos  # TrackLocation
         self.state = state  # int
 
@@ -169,15 +171,17 @@ class CarRaceInfo:
 
 #
 class PrivCar:
-    def __init__(self, driverIndex, moduleIndex, modName, corner, gear, gearNext, fuel, fuelConsumptionTotal,
-                 fuelConsumptionInstant, engineRPM, engineRPMRedLine, engineRPMMax, engineRPMMaxTq, engineRPMMaxPw,
-                 engineMaxTq, engineMaxPw, gearRatio, gearNb, gearOffset, skid, reaction, collision, simCollision,
-                 smoke, normal, collPos, damage, debug, collisionState, localPressure, driveSkill,
+    def __init__(self, driverIndex, moduleIndex, modName, corner1, corner2, corner3, corner4, gear, gearNext, fuel,
+                 fuelConsumptionTotal, fuelConsumptionInstant, engineRPM, engineRPMRedLine, engineRPMMax,
+                 engineRPMMaxTq, engineRPMMaxPw, engineMaxTq, engineMaxPw, gearRatio1, gearRatio2, gearRatio3,
+                 gearRatio4, gearRatio5, gearRatio6, gearRatio7, gearRatio8, gearRatio9, gearRatio10, gearNb,
+                 gearOffset, skid1, skid2, skid3, skid4, reaction1, reaction2, reaction3, reaction4, collision,
+                 simCollision, smoke, normal, collPos, damage, debug, collisionState, localPressure, driveSkill,
                  steerTqCenter, steerTqAlign, dashboardInstantNb, dashboardRequestNb, dashabordActiveItem):
         self.driverIndex = driverIndex  # int
         self.moduleIndex = moduleIndex  # int
         self.modName = modName  # string
-        self.corner = corner  # PosD[]
+        self.corner = [corner1, corner2, corner3, corner4]  # PosD[]
         self.gear = gear  # int
         self.gearNext = gearNext  # int
         self.fuel = fuel  # float
@@ -190,11 +194,12 @@ class PrivCar:
         self.engineRPMMaxPw = engineRPMMaxPw  # float
         self.engineMaxTq = engineMaxTq  # float
         self.engineMaxPw = engineMaxPw  # float
-        self.gearRatio = gearRatio  # float[]
+        self.gearRatio = [gearRatio1, gearRatio2, gearRatio3, gearRatio4, gearRatio5, gearRatio6, gearRatio7,
+                          gearRatio8, gearRatio9, gearRatio10]  # float[]
         self.gearNb = gearNb  # int
         self.gearOffset = gearOffset  # int
-        self.skid = skid  # float[]
-        self.reaction = reaction  # float[]
+        self.skid = [skid1, skid2, skid3, skid4]  # float[]
+        self.reaction = [reaction1, reaction2, reaction3, reaction4]  # float[]
         self.collision = collision  # int
         self.simCollision = simCollision  # int
         self.smoke = smoke  # float
@@ -232,11 +237,20 @@ class CollisionState:
         self.force = force  # Vector
 
 
-#
+# a vector
+class Vector:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+# info returned by the driver during a race
 class CarCtrl:
     def __init__(self, steer, accelCmd, brakeCmd, clutchCmd, brakeFrontLeftCmd, brakeFrontRightCmd, brakeRearLeftCmd,
                  brakeRearRightCmd, wingFrontCmd, wingRearCmd, reserved1, reserved2, gear, raceCmd, lightCmd, eBrakeCmd,
-                 wingControlMode, singleWheelBrakeMode, switch3, telemetryMode, msg, msgColor):
+                 wingControlMode, singleWheelBrakeMode, switch3, telemetryMode, msg1, msg2, msg3, msg4, msgColor1,
+                 msgColor2, msgColor3, msgColor4):
         self.steer = steer  # float
         self.accelCmd = accelCmd  # float
         self.brakeCmd = brakeCmd  # float
@@ -257,63 +271,90 @@ class CarCtrl:
         self.singleWheelBrakeMode = singleWheelBrakeMode  # int
         self.switch3 = switch3  # int
         self.telemetryMode = telemetryMode  # int
-        self.msg = msg  # string[]
-        self.msgColor = msgColor  # float[]
+        self.msg = [msg1, msg2, msg3, msg4]  # string[]
+        self.msgColor = [msgColor1, msgColor2, msgColor3, msgColor4]  # float[]
 
 
 # car setup parameters
 class CarSetup:
-    def __init__(self, fRWeightRep, fRLWeightRep, rRLWeightRep, fuel, wingAngle, revsLimiter, gearRatio,
-                 differentialType, differentialRatio, differentialMinTqBias, differentialMaxTqBias,
-                 differentialViscosity, differentialLockingTq, differentialMaxSlipBias, differentialCoastMaxSlipBias,
-                 steerLock, brakeRepartition, brakePressure, rideHeight, toe, camber, tirePressure, tireOpLoad,
-                 arbSprint, heaveBellCrank, heaveIntertance, heaveFastBump, heaveSlowBump, heaveBumpLevel,
-                 heaveFastRebound, heaveSlowRebound, heaveReboundLevel, suspSprint, suspBellCrank, suspInertance,
-                 suspCourse, suspPacker, suspFastBump, suspSlowBump, suspBumpLevel, suspFastRebound, suspSlowRebound,
-                 suspReboundLevel, reqRepair, reqTireset, reqPenalty):
+    def __init__(self, fRWeightRep, fRLWeightRep, rRLWeightRep, fuel, wingAngle1, wingAngle2, revsLimiter, gearRatio1,
+                 gearRatio2, gearRatio3, gearRatio4, gearRatio5, gearRatio6, gearRatio7, gearRatio8, gearRatio9,
+                 gearRatio10, differentialType1, differentialType2, differentialType3, differentialRatio1,
+                 differentialRatio2, differentialRatio3, differentialMinTqBias1, differentialMinTqBias2,
+                 differentialMinTqBias3, differentialMaxTqBias1, differentialMaxTqBias2, differentialMaxTqBias3,
+                 differentialViscosity1, differentialViscosity2, differentialViscosity3, differentialLockingTq1,
+                 differentialLockingTq2, differentialLockingTq3, differentialMaxSlipBias1, differentialMaxSlipBias2,
+                 differentialMaxSlipBias3, differentialCoastMaxSlipBias1, differentialCoastMaxSlipBias2,
+                 differentialCoastMaxSlipBias3, steerLock, brakeRepartition, brakePressure, rideHeight1, rideHeight2,
+                 rideHeight3, rideHeight4, toe1, toe2, toe3, toe4, camber1, camber2, camber3, camber4, tirePressure1,
+                 tirePressure2, tirePressure3, tirePressure4, tireOpLoad1, tireOpLoad2, tireOpLoad3, tireOpLoad4,
+                 arbSpring1, arbSpring2, arbBellCrank1, arbBellCrank2, heaveSpring1, heaveSpring2, heaveBellCrank1,
+                 heaveBellCrank2, heaveInertance1, heaveInertance2, heaveFastBump1, heaveFastBump2, heaveSlowBump1,
+                 heaveSlowBump2, heaveBumpLevel1, heaveBumpLevel2, heaveFastRebound1, heaveFastRebound2,
+                 heaveSlowRebound1, heaveSlowRebound2, heaveReboundLevel1, heaveReboundLevel2, suspSpring1, suspSpring2,
+                 suspSpring3, suspSpring4, suspBellCrank1, suspBellCrank2, suspBellCrank3, suspBellCrank4,
+                 suspInertance1, suspInertance2, suspInertance3, suspInertance4, suspCourse1, suspCourse2, suspCourse3,
+                 suspCourse4, suspPacker1, suspPacker2, suspPacker3, suspPacker4, suspFastBump1, suspFastBump2,
+                 suspFastBump3, suspFastBump4, suspSlowBump1, suspSlowBump2, suspSlowBump3, suspSlowBump4,
+                 suspBumpLevel1, suspBumpLevel2, suspBumpLevel3, suspBumpLevel4, suspFastRebound1, suspFastRebound2,
+                 suspFastRebound3, suspFastRebound4, suspSlowRebound1, suspSlowRebound2, suspSlowRebound3,
+                 suspSlowRebound4, suspReboundLevel1, suspReboundLevel2, suspReboundLevel3, suspReboundLevel4,
+                 reqRepair, reqTireset, reqPenalty):
         self.fRWeightRep = fRWeightRep  # CarSetupItem
         self.fRLWeightRep = fRLWeightRep  # CarSetupItem
         self.rRLWeightRep = rRLWeightRep  # CarSetupItem
         self.fuel = fuel  # CarSetupItem
-        self.wingAngle = wingAngle  # CarSetupItem[]
+        self.wingAngle = [wingAngle1, wingAngle2]  # CarSetupItem[]
         self.revsLimiter = revsLimiter  # CarSetupItem
-        self.gearRatio = gearRatio  # CarSetupItem[]
-        self.differentialType = differentialType  # int[]
-        self.differentialRatio = differentialRatio  # CarSetupItem[]
-        self.differentialMinTqBias = differentialMinTqBias  # CarSetupItem[]
-        self.differentialMaxTqBias = differentialMaxTqBias  # CarSetupItem[]
-        self.differentialViscosity = differentialViscosity  # CarSetupItem[]
-        self.differentialLockingTq = differentialLockingTq  # CarSetupItem[]
-        self.differentialMaxSlipBias = differentialMaxSlipBias  # CarSetupItem[]
-        self.differentialCoastMaxSlipBias = differentialCoastMaxSlipBias  # CarSetupItem[]
+        self.gearRatio = [gearRatio1, gearRatio2, gearRatio3, gearRatio4, gearRatio5, gearRatio6, gearRatio7,
+                          gearRatio8, gearRatio9, gearRatio10]  # CarSetupItem[]
+        self.differentialType = [differentialType1, differentialType2, differentialType3]  # int[]
+        self.differentialRatio = [differentialRatio1, differentialRatio2, differentialRatio3]  # CarSetupItem[]
+        self.differentialMinTqBias = [differentialMinTqBias1, differentialMinTqBias2,
+                                      differentialMinTqBias3]  # CarSetupItem[]
+        self.differentialMaxTqBias = [differentialMaxTqBias1, differentialMaxTqBias2,
+                                      differentialMaxTqBias3]  # CarSetupItem[]
+        self.differentialViscosity = [differentialViscosity1, differentialViscosity2,
+                                      differentialViscosity3]  # CarSetupItem[]
+        self.differentialLockingTq = [differentialLockingTq1, differentialLockingTq2,
+                                      differentialLockingTq3]  # CarSetupItem[]
+        self.differentialMaxSlipBias = [differentialMaxSlipBias1, differentialMaxSlipBias2,
+                                        differentialMaxSlipBias3]  # CarSetupItem[]
+        self.differentialCoastMaxSlipBias = [differentialCoastMaxSlipBias1, differentialCoastMaxSlipBias2,
+                                             differentialCoastMaxSlipBias3]  # CarSetupItem[]
         self.steerLock = steerLock  # CarSetupItem
         self.brakeRepartition = brakeRepartition  # CarSetupItem
         self.brakePressure = brakePressure  # CarSetupItem
-        self.rideHeight = rideHeight  # CarSetupItem[]
-        self.toe = toe  # CarSetupItem[]
-        self.camber = camber  # CarSetupItem[]
-        self.tirePressure = tirePressure  # CarSetupItem[]
-        self.tireOpLoad = tireOpLoad  # CarSetupItem[]
-        self.arbSprint = arbSprint  # CarSetupItem[]
-        self.heaveBellCrank = heaveBellCrank  # CarSetupItem[]
-        self.heaveIntertance = heaveIntertance  # CarSetupItem[]
-        self.heaveFastBump = heaveFastBump  # CarSetupItem[]
-        self.heaveSlowBump = heaveSlowBump  # CarSetupItem[]
-        self.heaveBumpLevel = heaveBumpLevel  # CarSetupItem[]
-        self.heaveFastRebound = heaveFastRebound  # CarSetupItem[]
-        self.heaveSlowRebound = heaveSlowRebound  # CarSetupItem[]
-        self.heaveReboundLevel = heaveReboundLevel  # CarSetupItem[]
-        self.suspSprint = suspSprint  # CarSetupItem[]
-        self.suspBellCrank = suspBellCrank  # CarSetupItem[]
-        self.suspInertance = suspInertance  # CarSetupItem[]
-        self.suspCourse = suspCourse  # CarSetupItem[]
-        self.suspPacker = suspPacker  # CarSetupItem[]
-        self.suspFastBump = suspFastBump  # CarSetupItem[]
-        self.suspSlowBump = suspSlowBump  # CarSetupItem[]
-        self.suspBumpLevel = suspBumpLevel  # CarSetupItem[]
-        self.suspFastRebound = suspFastRebound  # CarSetupItem[]
-        self.suspSlowRebound = suspSlowRebound  # CarSetupItem[]
-        self.suspReboundLevel = suspReboundLevel  # CarSetupItem[]
+        self.rideHeight = [rideHeight1, rideHeight2, rideHeight3, rideHeight4]  # CarSetupItem[]
+        self.toe = [toe1, toe2, toe3, toe4]  # CarSetupItem[]
+        self.camber = [camber1, camber2, camber3, camber4]  # CarSetupItem[]
+        self.tirePressure = [tirePressure1, tirePressure2, tirePressure3, tirePressure4]  # CarSetupItem[]
+        self.tireOpLoad = [tireOpLoad1, tireOpLoad2, tireOpLoad3, tireOpLoad4]  # CarSetupItem[]
+        self.arbSpring = [arbSpring1, arbSpring2]  # CarSetupItem[]
+        self.arbBellCrank = [arbBellCrank1, arbBellCrank2]  # CarSetupItem[]
+        self.heaveSpring = [heaveSpring1, heaveSpring2]  # CarSetupItem[]
+        self.heaveBellCrank = [heaveBellCrank1, heaveBellCrank2]  # CarSetupItem[]
+        self.heaveInertance = [heaveInertance1, heaveInertance2]  # CarSetupItem[]
+        self.heaveFastBump = [heaveFastBump1, heaveFastBump2]  # CarSetupItem[]
+        self.heaveSlowBump = [heaveSlowBump1, heaveSlowBump2]  # CarSetupItem[]
+        self.heaveBumpLevel = [heaveBumpLevel1, heaveBumpLevel2]  # CarSetupItem[]
+        self.heaveFastRebound = [heaveFastRebound1, heaveFastRebound2]  # CarSetupItem[]
+        self.heaveSlowRebound = [heaveSlowRebound1, heaveSlowRebound2]  # CarSetupItem[]
+        self.heaveReboundLevel = [heaveReboundLevel1, heaveReboundLevel2]  # CarSetupItem[]
+        self.suspSpring = [suspSpring1, suspSpring2, suspSpring3, suspSpring4]  # CarSetupItem[]
+        self.suspBellCrank = [suspBellCrank1, suspBellCrank2, suspBellCrank3, suspBellCrank4]  # CarSetupItem[]
+        self.suspInertance = [suspInertance1, suspInertance2, suspInertance3, suspInertance4]  # CarSetupItem[]
+        self.suspCourse = [suspCourse1, suspCourse2, suspCourse3, suspCourse4]  # CarSetupItem[]
+        self.suspPacker = [suspPacker1, suspPacker2, suspPacker3, suspPacker4]  # CarSetupItem[]
+        self.suspFastBump = [suspFastBump1, suspFastBump2, suspFastBump3, suspFastBump4]  # CarSetupItem[]
+        self.suspSlowBump = [suspSlowBump1, suspSlowBump2, suspSlowBump3, suspSlowBump4]  # CarSetupItem[]
+        self.suspBumpLevel = [suspBumpLevel1, suspBumpLevel2, suspBumpLevel3, suspBumpLevel4]  # CarSetupItem[]
+        self.suspFastRebound = [suspFastRebound1, suspFastRebound2, suspFastRebound3,
+                                suspFastRebound4]  # CarSetupItem[]
+        self.suspSlowRebound = [suspSlowRebound1, suspSlowRebound2, suspSlowRebound3,
+                                suspSlowRebound4]  # CarSetupItem[]
+        self.suspReboundLevel = [suspReboundLevel1, suspReboundLevel2, suspReboundLevel3,
+                                 suspReboundLevel4]  # CarSetupItem[]
         self.reqRepair = reqRepair  # CarSetupItem
         self.reqTireset = reqTireset  # CarSetupItem
         self.reqPenalty = reqPenalty  # CarSetupItem
@@ -336,7 +377,7 @@ class CarPitCmd:
         self.fuel = fuel  # float
         self.repair = repair  # int
         self.stopType = stopType  # int
-        self.setupChanges = setupChanged  # bool
+        self.setupChanged = setupChanged  # bool
         self.tireChange = tireChange  # bool
 
 
