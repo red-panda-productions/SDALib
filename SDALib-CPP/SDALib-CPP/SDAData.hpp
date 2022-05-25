e2fa3832368541ddbe0d3e16bffa9e8120691366
KungFuDonkey <sietze.riemersma@gmail.com>
2022-05-25 15:28:26 +0200

    added correct CMAKE_BUILD_TYPE option to test-linux

--- SDALib-CPP/SDALib-CPP/SDAData.hpp
+++ SDALib-CPP/SDALib-CPP/SDAData.hpp
@@ -2,9 +2,11 @@
 
 #include "SDStructs.hpp"
 
-struct SDAData {
+struct SDAData
+{
 public:
     SDAData(tCarElt *p_car, tSituation *p_situation, unsigned long p_tickCount);
+
     SDAData();
 
     tCarElt Car;
