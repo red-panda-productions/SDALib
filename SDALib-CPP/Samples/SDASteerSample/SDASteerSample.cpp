3cd0f32f68e371a39172b5908057c7918e30dc7f
suppergerrie2 <15769860+suppergerrie2@users.noreply.github.com>
2022-06-20 11:21:45 +0200

    Update copyright comments to be consistent with the other repositories

--- SDALib-CPP/Samples/SDASteerSample/SDASteerSample.cpp
+++ SDALib-CPP/Samples/SDASteerSample/SDASteerSample.cpp
@@ -1,6 +1,8 @@
-// This program has been developed by students from the bachelor Computer Science at
-// Utrecht University within the Software Project course.
-// © Copyright Utrecht University (Department of Information and Computing Sciences)
+/*
+ * This program has been developed by students from the bachelor Computer Science at
+ * Utrecht University within the Software Project course.
+ * © Copyright Utrecht University (Department of Information and Computing Sciences)
+ */
 
 #include "SteerSampleDriver.h"
 
