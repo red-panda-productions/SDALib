4a8df3811d5ff386c44cd991878f0498ed9138ba
suppergerrie2 <15769860+suppergerrie2@users.noreply.github.com>
2022-06-20 11:05:47 +0200

    Add copyright notice to project files

--- SDALib-CPP/SDALib-CPP/SDAData.cpp
+++ SDALib-CPP/SDALib-CPP/SDAData.cpp
@@ -1,3 +1,7 @@
+// This program has been developed by students from the bachelor Computer Science at
+// Utrecht University within the Software Project course.
+// © Copyright Utrecht University (Department of Information and Computing Sciences)
+
 #include "SDAData.hpp"
 
 SDAData::SDAData(tCarElt* p_car, tSituation* p_situation, unsigned long p_tickCount)
