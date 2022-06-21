5eb92c680343fb04b25d6f5476ac301054732f9f
Riemersma,S.N. (Sietze) <s.n.riemersma@students.uu.nl>
2022-06-21 22:46:42 +0200

    start of new SDATypes.py

--- SDALib-CPP/SDALib-Python/SDATypes.py
+++ SDALib-CPP/SDALib-Python/SDATypes.py
@@ -413,3 +413,349 @@ class SDAAction:
         self.accel = accel
         self.brake = brake
         self.clutch = clutch
+
+class Aero:
+    def __init__(self, drag, lift1, lift2, Clift1, Clift2, Cd, CdBody):
+        self.drag = drag              #float
+        self.lift = [lift1, lift2]    #float[2]
+        self.Clift = [Clift1, Clift2] #float[2]
+        self.Cd = Cd                  #float
+        self.CdBody = CdBody          #float
+
+class Wing:
+    def __init__(self, forces, Kx, Kz, Kz_org, angle, staticPos, AoAatMax, AoAatZero, AoAatZRad, AoAOffset, CliftMax, CliftZero, CliftAsymp,
+                 a, b, c, d, f, AoStall, Stallw, AR, Kx1, Kx2, Kx3, Kx4, Kz1, Kz2, Kz3, WingType):
+        self.forces = forces #Vector
+        self.Kx = Kx #float
+        self.Kz = Kz #float
+        self.Kz_org = Kz_org #float
+        self.angle = angle #float
+        self.staticPos = staticPos #Vector
+        self.AoAatMax = AoAatMax #float
+        self.AoAatZero = AoAatZero #float
+        self.AoAatZRad = AoAatZRad #float
+        self.AoAOffset = AoAOffset #float
+        self.CliftMax = CliftMax #float
+        self.CliftZero = CliftZero #float
+        self.CliftAsymp = CliftAsymp #float
+        self.a = a #float
+        self.b = b #float
+        self.c = c #float
+        self.d = d #float   YES NO e FOR SOME REASON
+        self.f = f #float
+        self.AoStall = AoStall #float
+        self.Stallw = Stallw #float
+        self.AR = AR #float
+        self.Kx1 = Kx1 #float
+        self.Kx2 = Kx2 #float
+        self.Kx3 = Kx3 #float
+        self.Kx4 = Kx4 #float
+        self.Kz1 = Kz1 #float
+        self.Kz2 = Kz2 #float
+        self.Kz3 = Kz3 #float
+        self.WingType = WingType #int
+
+
+class DemperDef:
+    def __init__(self, C1, b1, v1, C2, b2):
+        self.C1 = C1 #float
+        self.b1 = b1 #float
+        self.v1 = v1 #float
+        self.C2 = C2 #float
+        self.b2 = b2 #float
+
+class Demper:
+    def __init__(self, bump, rebound):
+        self.bumb = bump #DemperDef
+        self.rebound = rebound #DemperDef
+
+class Spring:
+    def __init__(self, K, F0, x0, xMax, bellcrank, packers):
+        self.K = K #float
+        self.F0 = F0 #float
+        self.x0 = x0 #float 
+        self.xMax = xMax #float
+        self.bellcrank = bellcrank #float
+        self.packers = packers #float
+
+class Suspension:
+    def __init__(self, spring, damper, inertance, x, v, a, force, state):
+        self.spring = spring #Spring
+        self.damper = damper #Damper
+        self.inertance = inertance #float
+        self.x = x #float
+        self.v = v #float
+        self.a = a #float
+        self.force = force #float
+        self.state = state #int
+
+class Brake:
+    def __init__(self, pressure, Tq, coeff, I, radius, temp, TCL, ABS, EnableABS):
+        self.pressure = pressure #float
+        self.Tq = Tq #float
+        self.coeff = coeff #float
+        self.I = I #float
+        self.radius = radius #float
+        self.temp = temp #float
+        self.TCL = TCL #float
+        self.ABS = ABS #float
+        self.EnableABS = EnableABS #bool
+
+class BrakeSyst:
+    def __init__(self, rep, coeff, ebrake_pressure):
+        self.rep = rep #float
+        self.coeff = coeff #float
+        self.ebrake_pressure = ebrake_pressure #float
+
+class DynAxis:
+    def __init__(self, spinVel, Tq, brkTq, I):
+        self.spinVel = spinVel #float
+        self.Tq = Tq #float
+        self.brkTq = brkTq #float
+        self.I = I #float
+
+class Differential:
+    def __init__(self, type, ratio, I, efficiency, bias, dTqMin, dTqMax, dSlipMax, dCoastSlipMax, lockInputTq, viscosity, viscomax, invar, feedback):
+        self.type = type #int
+        self.ratio = ratio #float
+        self.I = I #float
+        self.efficiency = efficiency #float
+        self.bias = bias #float
+        self.dTqMin = dTqMin #float
+        self.dTqMax = dTqMax #float
+        self.dSlipMax = dSlipMax #float
+        self.dCoastSlipMax = dCoastSlipMax #float
+        self.lockInputTq = lockInputTq #float
+        self.viscosity = viscosity #float
+        self.viscomax = viscomax #float
+        self.invar = invar #DynAxis
+        self.feedback = feedback #DynAxis
+
+        #2 variables not set but simu will set them in CarConstructor
+
+class Axle:
+    def __init__(self, xpos, arbSusp, heaveSusp, wheight0, force1, force2, I):
+        self.xpos = xpos #float
+        self.arbSusparbSusp = arbSusp #Suspension
+        self.heaveSusp = heaveSusp #Suspension
+        self.wheight0 = wheight0 #float
+        self.force = [force1, force2] #float[2]
+        self.I = I #float
+
+class Steer:
+    def __init__(self, steerLock, maxSpeed, steer):
+        self.steerLock = steerLock #float
+        self.maxSpeed = maxSpeed #float
+        self.steer = steer #float
+
+class Wheel:
+    def __init__(self, susp, brake, forces, torques, torqueAlign, roolRes, rideHeight, zRoad, pos, bodyVel, driveTq, vt, spinTq,
+                 spinVel, prespinVel, state, axleFz, axleFz3rd, trkPos, relPos, sa, sx, steer, staticPos, cosax, sinax, weight0, 
+                 tireSpringRate, radius, mu, I, curI, mfC, mfB, mfE, lfMax, lfMin, lfK, opLoad, AlignTqFactor, mass, camber, pressure,
+                 Ttire, Topt, Tinit, muTmult, heatingm, aircoolm, speedcoolm, wearrate, treadDepth, critTreadDepth, muTDmult1, muTDmult2,
+                 muTDoffset1, muTDoffset2, invar, feedBack, preFn, preFt):
+        self.susp = susp #Suspension
+        self.brake = brake #Brake
+        self.forces = forces #Vector
+        self.torques = torques #Vector
+        self.torqueAlign = torqueAlign #float
+        self.roolRes = roolRes #float
+        self.rideHeight = rideHeight #float
+        self.zRoad = zRoad #float
+        self.pos = pos #Vector
+        self.bodyVel = bodyVel #Vector
+        self.driveTq = driveTq #float
+        self.vt = vt #float
+        self.spinTq = spinTq #float
+        self.spinVel = spinVel #float
+        self.prespinVel = prespinVel #float
+        self.state = state #int
+        self.axleFz = axleFz #float
+        self.axleFz3rd = axleFz3rd #float
+        self.trkPos = trkPos #TrkLocPos
+        self.relPos = relPos #Posd
+        self.sa = sa #float
+        self.sx = sx #float
+        self.steer = steer #float
+        self.staticPos = staticPos #Posd
+        self.cosax = cosax #float
+        self.sinax = sinax #float
+        self.weight0 = weight0 #float
+        self.tireSpringRate = tireSpringRate #float
+        self.radius = radius #float#float
+        self.mu = mu #float
+        self.I = I #float
+        self.curI = curI #float
+        self.mfC = mfC #float
+        self.mfB = mfB #float
+        self.mfE = mfE #float
+        self.lfMax = lfMax #float
+        self.lfMin = lfMin #float
+        self.lfK = lfK #float
+        self.opLoad = opLoad #float
+        self.AlignTqFactor = AlignTqFactor #float
+        self.mass = mass #float
+        self.camber = camber #float
+        self.pressure = pressure #float
+        self.Ttire = Ttire #float
+        self.Topt = Topt #float
+        self.Tinit = Tinit #float
+        self.muTmult = muTmult #float
+        self.heatingm = heatingm #float
+        self.aircoolm = aircoolm #float
+        self.speedcoolm = speedcoolm #float
+        self.wearrate = wearrate #float
+        self.treadDepth = treadDepth #float
+        self.critTreadDepth = critTreadDepth #float
+        self.muTDmult = [muTDmult1, muTDmult2] #float[2]
+        self.muTDoffset = [muTDoffset1, muTDoffset2] #float[2]
+        self.invar = invar #DynAxis 
+        self.feedBack = feedBack #DynAxis
+        self.preFn = preFn #float
+        self.preFt = preFt #float
+
+class Gearbox:
+    def __init__(self, gear, gearMin, gearMax, gearNext, shiftTime, timeToEngage):
+        self.gear = gear #int
+        self.gearMin = gearMin #int
+        self.gearMax = gearMax #int
+        self.gearNext = gearNext #int
+        self.shiftTime = shiftTime #float
+        self.timeToEngage = timeToEngage #float
+
+class Clutch:
+    def __init__(self, state, mode, timeToRelease, releaseTime, transferValue):
+        self.state = state #int
+        self.mode = mode #int
+        self.timeToRelease = timeToRelease #float
+        self.releaseTime = releaseTime #float
+        self.transferValue = transferValue #float
+
+class Transmission:
+    def __init__(self, gearbox, clutch, overallRatio1, overallRatio2, overallRatio3, overallRatio4, overallRatio5, overallRatio6,
+                 overallRatio7, overallRatio8, overallRatio9, overallRatio10, gearI1, gearI2, gearI3, gearI4, gearI5, gearI6,
+                 gearI7, gearI8, gearI9, gearI10,  driveI1, driveI2, driveI3, driveI4, driveI5, driveI6,
+                 driveI7, driveI8, driveI9, driveI10, freeI1, freeI2, freeI3, freeI4, freeI5, freeI6,
+                 freeI7, freeI8, freeI9, freeI10, gearEff1, gearEff2, gearEff3, gearEff4, gearEff5, gearEff6,
+                 gearEff7, gearEff8, gearEff9, gearEff10, currI, differential1, differential2, differential3):
+        self.gearbox = gearbox
+        self.clutch = clutch
+        self.overallRatio = [overallRatio1, overallRatio2, overallRatio3, overallRatio4, overallRatio5, overallRatio6,
+                 overallRatio7, overallRatio8, overallRatio9, overallRatio10]
+        self.gearI = [gearI1, gearI2, gearI3, gearI4, gearI5, gearI6, gearI7, gearI8, gearI9, gearI10]
+        self.driveI = [driveI1, driveI2, driveI3, driveI4, driveI5, driveI6, driveI7, driveI8, driveI9, driveI10]
+        self.freeI = [freeI1, freeI2, freeI3, freeI4, freeI5, freeI6, freeI7, freeI8, freeI9, freeI10]
+        self.gearEff = [gearEff1, gearEff2, gearEff3, gearEff4, gearEff5, gearEff6, gearEff7, gearEff8, gearEff9, gearEff10]
+        self.currI = currI
+        self.differential = [differential1, differential2, differential3]
+
+
+
+#endif /* _TRANSMISSION_H_ */
+
+#ifndef _ENGINE_H_
+#define _ENGINE_H_
+
+typedef struct
+{
+    tdble rads;
+    tdble a;
+    tdble b;
+} tEngineCurveElem;
+
+typedef struct
+{
+    tdble maxTq;
+    tdble maxPw;
+    tdble rpmMaxPw;
+    tdble TqAtMaxPw;
+    tdble rpmMaxTq;
+    int nbPts;
+    tEngineCurveElem* data;
+} tEngineCurve;
+
+typedef struct
+{
+    tEngineCurve curve;
+    tdble revsLimiter;
+    tdble revsMax;
+    tdble tickover;
+    tdble I;
+    tdble rads;        /* revs in rad/s ... */
+    tdble Tq;          /* output torque */
+    tdble Tq_response; /* response Tq due to mismatch */
+    tdble I_joint;     /* joint inertia */
+    tdble fuelcons;
+    tdble brakeCoeff;    /* coefficient for constant engine brake */
+    tdble brakeLinCoeff; /* coefficient for RPM dependent engine brake */
+    tdble pressure;
+    tdble exhaust_pressure;
+    tdble exhaust_refract;
+    tdble timeInLimiter; /* time to still spend with fuel cut, in secundum */
+
+    tdble TCL;       // Optional TCL
+    bool EnableTCL;  // Enable optional TCL
+} tEngine;
+
+#endif /* _ENGINE_H_ */ 
+
+typedef struct
+{
+    /* driver's interface */
+    tCarCtrl* ctrl;
+    void* params;
+    tCarElt* carElt;
+
+    tCarCtrl preCtrl;
+
+    /* components */
+    tAxle axle[2];
+    tWheel wheel[4];
+    tSteer steer;
+    tBrakeSyst brkSyst;
+    tAero aero;
+    tWing wing[2];
+    tTransmission transmission; /* includes clutch, gearbox and driveshaft */
+    tEngine engine;
+
+    /* static */
+    t3Dd dimension; /* car's mesures */
+    tdble mass;     /* mass with pilot (without fuel) */
+    tdble Minv;     /* 1 / mass with pilot (without fuel) */
+    tdble tank;     /* fuel tank capa */
+    t3Dd statGC;    /* static pos of GC */
+    t3Dd Iinv;      /* inverse of inertial moment along the car's 3 axis */
+
+    /* dynamic */
+    tdble fuel;             /* current fuel load */
+    tdble fuel_consumption; /* average fuel consumption */
+    tdble fuel_prev;        /* average fuel consumption */
+    tdble fuel_time;        /* average fuel consumption */
+    tDynPt DynGC;           /* GC local data except position */
+    tDynPt DynGCg;          /* GC global data */
+    tPosd VelColl;          /* resulting velocity after collision */
+    tDynPt preDynGC;        /* previous one */
+    tTrkLocPos trkPos;      /* current track position */
+    tdble airSpeed2;        /* current air speed (squared) for aerodynamic forces */
+
+    /* internals */
+    tdble Cosz;
+    tdble Sinz;
+    tDynPt corner[4]; /* x,y,z for static relative pos, ax,ay,az for dyn. world coord */
+    int collision;
+    t3Dd normal;
+    t3Dd collpos;
+    tdble wheelbase;
+    tdble wheeltrack;
+    sgMat4 posMat;
+    DtShapeRef shape; /* for collision */
+    int blocked;      // Flag to show if the car has had already a collision in the same timestep.
+    int dammage;
+
+    /* enabling features */
+    int features;
+
+    tDynPt restPos; /* target rest position after the car is broken */
+
+    int collisionAware;
+} tCar;
